fff9f54256c3443d4290b656916413d30fa607be
agungshndy <agungshandy43@gmail.com>
2024-12-09 12:24:07 +0700

    home updates

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,23 @@
+import { useState } from 'react';
+
 function Home(){
+
+    const [count,setCount] = useState(0);
+
+    const increment = () => {
+        setCount(count => count + 1)
+    }
+
+    const decrement = () => {
+        setCount(count => count - 1)
+    }
 
     return (
         <>
         <h1>Hello World!</h1>
+        <h2>{count}</h2>
+        <button onClick={decrement} className='gap-2'>-</button>
+        <button onClick={increment}>+</button>
         </>
     )
 }
